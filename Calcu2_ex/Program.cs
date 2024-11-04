aca7b24dff43135c2a3674a8d4f84170c83fa064
남정욱 <wjddnr3304@gmail.com>
2024-11-04 14:45:38 +0900

    초기 커밋.

---
+++ Calcu2_ex/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Calcu2_ex
+{
+     class Program
+    {
+        static double Calcu(double a, string op, double b)
+        {
+            double r = 0;
+            switch (op)
+            {
+                case "+":
+                    r = a + b;
+                    break;
+                case "-":
+                    r = a - b;
+                    break;
+                case "*":
+                    r = a * b;
+                    break;
+                case "/":
+                    r = a / b;
+                    break;
+            }
+            return r;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("계속 진행할까요?(중지를 원하시면 q 를 입력하세요)");
+                string quit = Console.ReadLine();
+                if (quit == "q") break;
+                Console.Write("값1?");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("연산(+,-,*,/)?");
+                string o = Console.ReadLine();
+                Console.Write("값2?");
+                double y = double.Parse(Console.ReadLine());
+                double result = Calcu(x, o, y);
+                Console.WriteLine("={0,2:F}", result);
+            }
+
+        }
+    }
+}
